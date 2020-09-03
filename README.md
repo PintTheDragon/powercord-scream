db2e49b567cdad151424e89e47359b28fef059b3
PintTheDragon <PintTheDragon@hotmail.com>
2020-09-03 14:46:29 -0700

    Let people scream!

---
+++ README.md
@@ -0,0 +1,8 @@
+# Scream
+Scream is a powercord module to let you scream with a message! To use it, put the plugin in your plugins folder. AahaAhAAAHhAahaaAHAaAhhaAhAAAHahAhAAahaA
+
+## Commands
+Remember, all of these commands need to start with your powercord command prefix (replace ``.`` with whatever your prefix is)!
+
+- ``.scream <message>`` - Scream
+- ``.descream <message>`` - Figure out the message behind someone's scream
---
+++ index.js
@@ -0,0 +1,49 @@
+const { Plugin } = require('powercord/entities');
+
+module.exports = class Scream extends Plugin {
+  async startPlugin () {
+	  powercord.api.commands.registerCommand({
+      command: 'scream',
+      description: 'Scream your message',
+      usage: '{c} <message>',
+      executor: (args) => ({send: true, result: encode(args.join(" "))})
+    });
+	
+	powercord.api.commands.registerCommand({
+      command: 'descream',
+      description: 'Decode a scream',
+      usage: '{c} <message>',
+      executor: (args) => ({send: true, result: decode(args.join(" "))})
+    });
+  }
+  
+}
+
+function encode1(input) {
+	let out = "";
+	
+	[...input].forEach((item) => {
+		out+=item.charCodeAt(0).toString(4).padStart(4, "0");
+	});
+	
+	return out;
+}
+
+function decode1(input){
+	let split = ((a,b)=>Array.from({length:Math.ceil(a.length/b)},(e,r)=>a.slice(r*b,r*b+b)))(input, 4);
+	let out = "";
+	
+	split.forEach((item) => {
+		out+=String.fromCharCode(parseInt(item, 4));
+	});
+	
+	return out;
+}
+
+function encode(input){
+	return encode1(input).replace(/3/g, "H").replace(/2/g, "h").replace(/1/g, "A").replace(/0/g, "a");
+}
+
+function decode(input){
+	return decode1(input.replace(/H/g, "3").replace(/h/g, "2").replace(/A/g, "1").replace(/a/g, "0"));
+}
